883ab3bf37db26b4e7ad85cca3df9a8467a20bed
anais <anais.meresse02450@gmail.com>
2021-10-04 13:48:27 +0200

    initialisation du portefolio

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// const PAPER = anime({
+//     ...GLOBAL_CFG,
+//     targets: "#paper1",
+//     delay: 500,
+//     duration: 3500,
+//     scaleY: [0, 0.6],
+//     scaleX: [0, 0.6],
+//     translateX: [-200, -100],
+//     translateY: [-200, -100],
+//     rotate: getRndInteger(-400, -100),
+//     opacity: [0.3, 0.7, 0],
+//     easing: "easeInOutSine",
+//   });
