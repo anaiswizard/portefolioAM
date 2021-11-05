e27b1175ea6fcb2a3b0188b5608f61be04bf3cdd
anais <anais.meresse02450@gmail.com>
2021-11-05 10:49:49 +0100

    finalisation de la V1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
-// const PAPER = anime({
-//     ...GLOBAL_CFG,
-//     targets: "#paper1",
-//     delay: 500,
-//     duration: 3500,
-//     scaleY: [0, 0.6],
-//     scaleX: [0, 0.6],
-//     translateX: [-200, -100],
-//     translateY: [-200, -100],
-//     rotate: getRndInteger(-400, -100),
-//     opacity: [0.3, 0.7, 0],
-//     easing: "easeInOutSine",
-//   });
+let button = document.querySelector(".btn-selection");
+console.log(button);
+let body = document.querySelector("body");
+button.addEventListener("click", (e) => {
+  if (body.classList.contains("pink")) {
+    body.classList.remove("pink");
+    body.classList.add("white");
+  } else {
+    body.classList.remove("white");
+    body.classList.add("pink");
+  }
+});
+
+let cardsBody = document.querySelectorAll(".card-body");
+console.log(cardsBody);
+
+button.addEventListener("click", (e) => {
+  for (let cardBody of cardsBody) {
+    if (cardBody.classList.contains("bg-black")) {
+      cardBody.classList.remove("bg-black");
+      cardBody.classList.add("pinkCard");
+    } else {
+      cardBody.classList.remove("pinkCard");
+      cardBody.classList.add("bg-black");
+    }
+  }
+});
